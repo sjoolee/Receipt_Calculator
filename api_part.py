8a84d0a7a8966470350b0b375f72f5eeed8de75a
Subin <113068909+sjoolee@users.noreply.github.com>
2025-03-23 16:48:38 -0400

    Update api_part.py

--- api_part.py
+++ api_part.py
@@ -2,18 +2,6 @@ import requests
 import re
 import pandas as pd
 
-img = 'IMAGE.jpg'
-
-api = 'https://api.api-ninjas.com/v1/imagetotext'
-
-
-api_url = 'https://api.api-ninjas.com/v1/imagetotext'
-image_file_descriptor = open(img, 'rb')
-files = {'image': image_file_descriptor}
-r = requests.post(api_url, files=files, headers={'X-Api-Key': 'YOUR_API_KEY'})
-#print(r.json())
-
-
 def correct_ocr_errors(text):
     """Fix common OCR errors in extracted text."""
     if isinstance(text, list):  
@@ -39,11 +27,11 @@ def extract_prices(ocr_response):
 
     # Define reject keywords regex for filtering out unwanted content
     reject_keywords = re.compile(
-        r'\b(Field|HWY|Street|Road|Ave|Blvd)\b|\d{1,2}/\d{1,2}/\d{4}|\(\d{3}\)\s?\d{3}-\d{4}|[A-Za-z]+\.[A-Za-z]+',
+        r'\b(Field|HWY|Street|Road|Ave|Blvd|TIP)\b|\d{1,2}/\d{1,2}/\d{4}|\(\d{3}\)\s?\d{3}-\d{4}|[A-Za-z]+\.[A-Za-z]+',
         re.IGNORECASE
     )
 
-    common_labels_pattern = re.compile(r'\b(?:' + '|'.join(re.escape(label) for label in ["Table", "Dine In", "Take", "Order", "Check"]) + r')\b', re.IGNORECASE)
+    common_labels_pattern = re.compile(r'\b(?:' + '|'.join(re.escape(label) for label in ["RDER:", "Table", "Dine In", "Take", "Order", "Check"]) + r')\b', re.IGNORECASE)
 
     for line in lines:
         print(f"Processing line: {line}")
@@ -75,7 +63,6 @@ def extract_prices(ocr_response):
     return items_prices
 
 
-
 def comparison(response_data):
     extracted_text = "\n".join([item["text"] for item in response_data]) 
     matches = re.findall(r'(.+)\s*[-]?\s*\$?(\d+\.\d{2})', extracted_text)
@@ -83,17 +70,5 @@ def comparison(response_data):
 
     return(compared_receipt_data)
 
-response_data = r.json()
 
-
-receipt_data = extract_prices(response_data)
-
-print('receipt data:')
-print(receipt_data)
-
-df = pd.DataFrame(receipt_data.items(), columns=['Item', 'Price'])
-compared_receipt = comparison(response_data)
-
-print(df)
-print(compared_receipt)
     
